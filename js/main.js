563ac05cd3e1f90cfa21669a213c973f3e0e191c
Dave Asay <dvasay@gmail.com>
2018-02-14 09:28:30 -0700

    test

--- js/main.js
+++ js/main.js
@@ -1,3 +1,34 @@
+var userChoice = prompt("Do you choose rock, paper, or scissors?");
+var computerChoice = Math.random();
+if (computerChoice < 0.33) {
+  computerChoice = "rock";
+} else if (computerChoice <= 0.67) {
+  computerChoice = "paper";
+} else {
+    computerChoice = "scissors";
+}
+var userChoice = prompt("Do you choose rock, paper, or scissors?");
+var computerChoice = Math.random();
+if (computerChoice < 0.34) {
+  computerChoice = "rock";
+} else if (computerChoice <= 0.67) {
+  computerChoice = "paper";
+} else {
+    computerChoice = "scissors";
+}
+
+var compare = function(choice1, choice2) {
+    if(choice1 === choice2) {
+    return "The result is a tie!";
+    }
+};
+
+
+
+
+
+/* Dave's code
+
 function startGame(e) {
   var choice = e.target.id
   console.log(choice)
@@ -11,3 +42,4 @@ for (var i in choices) {
     //First load
   }
 }
+*/
